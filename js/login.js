3cd333b10abdb1490ed8ed9c7a996c57ffa5bbf8
mamun-coderman <mamun.coderman@gmail.com>
2026-05-15 23:59:24 +0600

    login js code completed

--- js/login.js
+++ js/login.js
@@ -3,6 +3,21 @@
 
 
 document.getElementById("btn").addEventListener("click", function() {
-    console.log("Button clicked");
+
+    // pre-stored mobile number and pin
+    const mobileNumber = "01515295665";
+    const password = "1234";
+
+    // input mobile number and pin selection
+    const inputMobileNumber = document.getElementById("mobile-number").value;
+    const inputPin = document.getElementById("pin-number").value;
+
+    // compare both mobile number and pin equality
+    if( (mobileNumber === inputMobileNumber) && (password === inputPin) ) {
+        window.location.href="./home.html";
+    }
+    else{
+        alert("Invalid mobile number or password");
+    }
 });
 
