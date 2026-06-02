fd8f93ed030b1d1957706a9ff5949fa433f41c19
mamun-coderman <mamun.coderman@gmail.com>
2026-06-02 19:07:36 +0600

    toggling part js complete

--- js/home.js
+++ js/home.js
@@ -8,13 +8,43 @@ document.getElementById("logout-btn").addEventListener("click", function(){
 // card section event listener 
 // toggling function
 function togglingCard(cardId, formId) {
-    const cardSelection = document.getElementById(cardId)
-    cardSelection.addEventListener("click", function(){
-        const addMoneyForm = document.getElementById(formId);
-        addMoneyForm.style.display = "block";
-        cardSelection.style.backgroundColor = "#0874f20d"
-        cardSelection.style.border = " 1px solid #0874f2"
+    const cardToClick = document.getElementById(cardId)
+    cardToClick.addEventListener("click", function(){
+        //hiding all other form without clicked related form
+        const hidingForms = document.querySelectorAll(".form-section");
+        hidingForms.forEach(function(form) {
+            form.style.display = "none";
+        })
+
+        // all cards default color
+        const cardsDefaultColor = document.querySelectorAll(".card-area");
+        cardsDefaultColor.forEach(function(card) {
+            card.style.border = "1px solid #0808081a"
+            card.style.backgroundColor = "white";
+        })
+
+        // showing particular form with click
+        const showingForm = document.getElementById(formId);
+        showingForm.style.display = "block";
+        cardToClick.style.backgroundColor = "#0874f20d";
+        cardToClick.style.border = " 1px solid #0874f2";
     })
 }
 
+// toggling function calling for add money
 togglingCard("add-money", "add-money-form");
+
+// toggling function calling for cash out
+togglingCard("cash-out-card", "cash-out-form");
+
+// toggling function calling for transfer money
+togglingCard("transfer-money-card", "transfer-money-form");
+
+// toggling function calling for get bonus
+togglingCard("get-bonus-card", "get-bonus-form");
+
+// toggling function calling for pay bill
+togglingCard("pay-bill-card", "pay-bill-form");
+
+// toggling function calling for transaction history
+togglingCard("transaction-card", "transaction-form");
